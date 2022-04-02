d745ce7f0512933895a061746d3420858ee5c071
Artem Simanov <metall704@yandex.ru>
2022-04-02 10:35:58 +0300

    перенесено в сборщик

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -3,3 +3,27 @@
 $("document").ready(function() {
 	console.log('hello world');
 });
+let tabs = document.querySelectorAll('.tab')
+ 
+let conteinerItems = document.querySelectorAll('.container_item_text');
+
+
+let changeTabs = function (item, cI) {
+    
+    item.addEventListener('click', function () {
+        for (let tab of tabs) {
+            tab.classList.remove('tab_active')
+        }
+        item.classList.add('tab_active');
+
+        for (let conteinerItem of conteinerItems) {
+            conteinerItem.classList.remove('container_item__visible')
+        }
+        cI.classList.add('container_item__visible');        
+    })
+};
+    
+for (let i = 0; i < tabs.length; i++) {
+    changeTabs(tabs[i], conteinerItems[i])
+};
+
