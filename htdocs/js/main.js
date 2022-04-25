a1d5c55faaf8e181df4a54406697e42674bc6301
Artem Simanov <metall704@yandex.ru>
2022-04-25 20:19:53 +0300

    добавил брейкпоинты

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,7 +1,24 @@
 'use strict'
 
-$("document").ready(function() {
-	console.log('hello world');
+$('.rates').slick({
+	infinite: false,
+	centerMode: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	centerPadding: '20px',
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1,
+			  infinite: true,
+			  dots: true,
+			  centerPadding: 0,
+			  dots: false
+			}
+		},
+	]
 });
 let tabs = document.querySelectorAll('.tabs-switcher__btn')
  
