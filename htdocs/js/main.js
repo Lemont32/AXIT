31156af554b8d42665b53bd18e52d2c1426fbcfc
Artem Simanov <metall704@yandex.ru>
2022-05-02 14:42:11 +0300

    заменил slick на swiper

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,25 +1,23 @@
-'use strict'
-
-$('.rates').slick({
-	infinite: false,
-	centerMode: true,
-	slidesToShow: 3,
-	slidesToScroll: 3,
-	centerPadding: '20px',
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-			  slidesToShow: 1,
-			  slidesToScroll: 1,
-			  infinite: true,
-			  dots: true,
-			  centerPadding: 0,
-			  dots: false
-			}
-		},
-	]
-});
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    breakpoints: {
+        // when window width is >= 320px
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 10
+        }
+    }
+  });
 let tabs = document.querySelectorAll('.tabs-switcher__btn')
  
 let conteinerItems = document.querySelectorAll('.tabs-content__item');
--- src/modules/main.js
+++ src/modules/main.js
@@ -1,22 +1,20 @@
-'use strict'
-
-$('.rates').slick({
-	infinite: false,
-	centerMode: true,
-	slidesToShow: 3,
-	slidesToScroll: 3,
-	centerPadding: '20px',
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-			  slidesToShow: 1,
-			  slidesToScroll: 1,
-			  infinite: true,
-			  dots: true,
-			  centerPadding: 0,
-			  dots: false
-			}
-		},
-	]
-});
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    breakpoints: {
+        // when window width is >= 320px
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 10
+        }
+    }
+  });
